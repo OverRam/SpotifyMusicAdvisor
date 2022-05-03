10a1b6c9e8d6fb796b5b6a5b9314c56ca408149a
OverRam <yugini666@gmail.com>
2022-05-03 18:10:27 +0200

    Refactor

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -1,25 +1,30 @@
 package advisor;
 
 import advisor.Controller.ApplicationController;
-import advisor.Model.SpotifyApiConnectionModel;
+import advisor.Model.SpotifyAuth;
 import advisor.View.UserView;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ApplicationController controllerApplication = new ApplicationController(new UserView()
-                , SpotifyApiConnectionModel.getInstance());
+                , SpotifyAuth.getInstance());
 
-        if (args.length == 2 && args[0].equalsIgnoreCase("-access")) {
-            controllerApplication.setResourceUrl(args[1]);
+        if (args.length >= 2) {
+            HashMap<String, String> initParamsAuthAndResourceServer = new HashMap<>();
+            for (int i = 0; i < args.length; i += 2) {
+                initParamsAuthAndResourceServer.put(args[i], args[i + 1]);
+            }
+            controllerApplication.setInitParams(initParamsAuthAndResourceServer);
         }
 
-        String inputOption = "";
-        while (!inputOption.equalsIgnoreCase("exit"))  {
-            inputOption = sc.nextLine();
+        String inputOption = sc.nextLine();
+        while (!inputOption.equalsIgnoreCase("exit")) {
             controllerApplication.mainMenu(inputOption);
+            inputOption = sc.nextLine();
         }
 
     }
