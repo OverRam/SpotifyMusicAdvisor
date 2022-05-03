179572c2716a9577c645dfd19a7bb88be135614d
OverRam <yugini666@gmail.com>
2022-05-03 18:08:41 +0200

    Add method printPage

--- Music Advisor/task/src/advisor/View/UserView.java
+++ Music Advisor/task/src/advisor/View/UserView.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 public class UserView {
 
-    public void printMessage(List<String> messageList) {
+    public void printListOfMessage(List<String> messageList) {
         messageList.forEach(System.out::println);
     }
 
-    public void printMessage(String message) {
+    public void printListOfMessage(String message) {
         System.out.println(message);
+    }
+
+    public void printPage(List<String> contentList, int numOfPage, int numberOfPages) {
+        System.out.println("---PAGE " + numOfPage + " OF " + numberOfPages + "---");
+        printListOfMessage(contentList);
     }
 }
