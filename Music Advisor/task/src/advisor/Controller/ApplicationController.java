824e8730e95650dde6329a21612b87e55a12746d
OverRam <yugini666@gmail.com>
2022-05-06 14:07:09 +0200

    Refactor, add methods naviPage, menu, checkParamsAuth, setUserInputOption, runApplication

--- Music Advisor/task/src/advisor/Controller/ApplicationController.java
+++ Music Advisor/task/src/advisor/Controller/ApplicationController.java
@@ -1,20 +1,41 @@
 package advisor.Controller;
 
-import advisor.Model.DataBase;
+import advisor.Model.DataModel;
 import advisor.Model.SpotifyApiResourceModel;
 import advisor.Model.SpotifyAuth;
 import advisor.View.UserView;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class ApplicationController {
-    private final UserView userView;
-    private final SpotifyAuth spotifyAuth;
-    private boolean firstRun = true;
+    private final UserView userView = new UserView();
+    private final SpotifyAuth spotifyAuth = SpotifyAuth.getInstance();
+    private final DataModel dataModel = new DataModel();
+    private String userInputOption;
+    private boolean isAccess = false;
+    private boolean isNotExit = true;
 
-    public ApplicationController(UserView userView, SpotifyAuth spotifyAuth) {
-        this.userView = userView;
-        this.spotifyAuth = spotifyAuth;
+    public void runApplication() {
+        setUserInputOption();
+        String[] userCategoryChoice;
+
+        while (isNotExit) {
+            isNotExit = !userInputOption.equalsIgnoreCase("exit");
+            userCategoryChoice = userInputOption.split(" ");
+
+            if (!isAccess) {
+                checkParamsAuth(userInputOption);
+                setUserInputOption();
+            } else {
+                menu(userCategoryChoice);
+                setUserInputOption();
+            }
+        }
+    }
+
+    public void setUserInputOption() {
+        this.userInputOption = new Scanner(System.in).nextLine();
     }
 
     public void setInitParams(HashMap<String, String> params) {
@@ -25,67 +46,85 @@ public class ApplicationController {
         if (params.containsKey("-resource")) {
             SpotifyApiResourceModel.getInstance().setUrlHostApiResource(params.get("-resource"));
         }
-    }
 
-    public void mainMenu(String inputOption) {
-        SpotifyApiResourceModel apiResourceModel = SpotifyApiResourceModel.getInstance();
-        String[] userCategoryChoice = inputOption.split(" ");
-        String categoryName = "";
-
-        if (userCategoryChoice.length > 1) {
-            categoryName = inputOption.replaceAll(userCategoryChoice[0] + " ", "");
+        if (params.containsKey("-page")) {
+            try {
+                int numOfEntries = Integer.parseInt(params.get("-page"));
+                dataModel.setEntriesPerPage(numOfEntries);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
         }
 
-        if (userCategoryChoice[0].equalsIgnoreCase("auth")) {
+    }
+
+    public void checkParamsAuth(String inputOption) {
+        if (inputOption.equalsIgnoreCase("auth")) {
             setAuthorizationCode();
+
             if (spotifyAuth.isCode()) {
                 setAccessToken();
             }
-
             if (spotifyAuth.isAccessToken()) {
                 userView.printListOfMessage("Success!");
+                isAccess = true;
+                dataModel.setCategories();
             } else {
                 userView.printListOfMessage("Failed!");
             }
-        } else if (spotifyAuth.isAccessToken()) {
-
-            if (firstRun) {
-                apiResourceModel.getCategories("categories");
-                firstRun = false;
-            }
-
-            switch (userCategoryChoice[0].toLowerCase()) {
-                case "new":
-                    userView.printListOfMessage(apiResourceModel.getNewSongs("new-releases"));
-                    break;
-
-                case "featured":
-                    userView.printListOfMessage(apiResourceModel.getFeaturedPlaylists("featured-playlists"));
-                    break;
-
-                case "categories":
-                    userView.printListOfMessage(apiResourceModel.getCategories("categories"));
-                    break;
-
-                case "playlists":
-                    String categoryId = DataBase.geInstance().getCategoryID(categoryName);
-                    userView.printListOfMessage(apiResourceModel.getPlaylistCategoryID(categoryId));
-                    break;
-
-                case "exit":
-                    break;
-
-                default:
-                    userView.printListOfMessage("Wrong user request! Try again.");
-            }
-
         } else {
             userView.printListOfMessage("Please, provide access for application.");
         }
     }
 
-    private void setAuthorizationCode() {
+    void menu(String[] userCategoryChoice) {
+        String categoryName = "";
 
+        if (userCategoryChoice.length > 1) {
+            categoryName = userInputOption.replaceAll(userCategoryChoice[0] + " ", "");
+        }
+
+        switch (userCategoryChoice[0].toLowerCase()) {
+            case "new":
+                userView.printListOfMessage(dataModel.getNewSongs());
+                naviPage();
+                break;
+
+            case "featured":
+                userView.printListOfMessage(dataModel.getPlaylistsPage("featured-playlists"));
+                naviPage();
+                break;
+
+            case "categories":
+                userView.printListOfMessage(dataModel.getCategoryPage());
+                naviPage();
+                break;
+
+            case "playlists":
+                String categoryId = dataModel.getCategoryID(categoryName);
+
+                if (categoryId != null) {
+                    String url = "categories/" + categoryId + "/playlists";
+                    userView.printListOfMessage(dataModel.getPlaylistsPage(url));
+                    naviPage();
+                } else {
+                    userView.printListOfMessage("Wrong spotify ID.");
+                    setUserInputOption();
+                }
+                break;
+
+            case "exit":
+                userView.printListOfMessage(" ---GOODBYE!---");
+                break;
+
+            default: {
+                userView.printListOfMessage("Wrong user request! Try again.");
+            }
+
+        }
+    }
+
+    private void setAuthorizationCode() {
         userView.printListOfMessage("use this link to request the access code:");
         userView.printListOfMessage(spotifyAuth.getLinkToRequestAccessCode());
 
@@ -102,5 +141,21 @@ public class ApplicationController {
     private void setAccessToken() {
         userView.printListOfMessage("Making http request for access_token...");
         spotifyAuth.getAccessTokenFromServer();
+    }
+
+    private void naviPage() {
+        boolean isNextOrPrev;
+        boolean isExit = false;
+        do {
+            setUserInputOption();
+            isNextOrPrev = userInputOption.equals("next") || userInputOption.equals("prev");
+
+            if (isNextOrPrev) {
+                userView.printListOfMessage(dataModel.getOtherPage(userInputOption));
+            } else if (userInputOption.equalsIgnoreCase("exit")) {
+                isExit = true;
+                userInputOption = "";
+            }
+        } while (!isExit);
     }
 }
